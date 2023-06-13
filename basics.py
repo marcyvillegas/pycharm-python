38369a8fe2484c1bd92373781c218f046c413dff
Marcy Villegas <marcyvillegas17@gmail.com>
2023-06-13 14:08:34 +0800

    Added basics of python collections

---
+++ basics.py
@@ -0,0 +1,19 @@
+# -- LIST --
+mylist = ["apple", "banana", "cherry"]
+
+# -- TUPLES --
+mytuple = ("apple", "banana", "cherry")
+
+# -- DICTIONARIES --
+wife = {'name': 'Rose', 'age': 33}
+
+print(wife.get("none", "sample"))
+print(wife.get("name"))
+
+# -- SETS --
+set_1 = {1, 2, 3}
+# set_2 = set([1, 2, 3])
+
+set_1.add(4)
+print(set_1)
+
